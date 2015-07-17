d448db95a0ca72e11d6367807ed66fc164409ac7
rob perez <ptelerez@cox.net>
2015-07-17 18:35:07 +0000

    moved the new section form to a modal on the instructor course show page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ namespace :instructor do
     resources :lessons, :only => [:new, :create]
   end
   resources :courses, :only => [:new, :create, :show] do
-    resources :sections, :only => [:new, :create]
+    resources :sections, :only => [:create]
     end
 end
 
